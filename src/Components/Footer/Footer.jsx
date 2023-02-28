86bd253ab3f714d383a2d83290608f87d10171f6
Eduardo Leite <94940400+oeduardodev@users.noreply.github.com>
2023-02-28 09:55:23 -0300

    Alteração no Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,10 +8,10 @@ function Footer (){
             <div className={style.Contato}>
             <h4>Clique e Acesse:</h4>
             <li className={style.ItenContato}><BsLinkedin className={style.Icone}/> <a target="_blank"  href="https://www.linkedin.com/in/eduardo-leite-78620b1a3/">Meu Linkedin </a></li>
-            <li className={style.ItenContato}><BsGithub className={style.Icone}/> <a target="_blank" href="https://github.com/jovemscript/"> Meu GitHub </a></li>
+            <li className={style.ItenContato}><BsGithub className={style.Icone}/> <a target="_blank" href="https://portfolio-em-react-js.vercel.app/"> Meu GitHub </a></li>
             </div>
             <div className={style.Dev}>
-            <p>Copyright by Eduardo Leite <br/>24 de janeiro de 2023</p>
+            <p>Copyright by Eduardo Leite <br/>25 de fevereiro de 2023</p>
             </div>
         </footer>
     )

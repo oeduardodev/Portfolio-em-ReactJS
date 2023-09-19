708394c43cb4cb4ed5501b826fd2cd790617b5d6
oeduardodev <leite.eduardo@academico.ifpb.edu.br>
2023-09-19 16:35:26 -0300

    Atualizando icones.

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -14,8 +14,8 @@ function Sobre() {
           Sou extremamente curioso e apaixonado por tecnologia e artes digitais.
           Atualmente, estou focado em maximizar meus conhecimentos em React.JS e
           Node.JS. No entanto, estou sempre disposto a aprender novas
-          tecnologias, vale ressaltar que sempre tive muito interesse pela area
-          de suporte tecnico e relacionamento ao cliente. Ainda estou em busca
+          tecnologias, vale ressaltar que sempre tive muito interesse pela área
+          de suporte tecnico e relacionamento com cliente. Ainda estou em busca
           da minha primeira oportunidade sólida e duradoura na área da
           tecnologia.
         </p>
